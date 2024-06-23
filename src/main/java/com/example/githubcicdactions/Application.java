9fcfd602d7a9ebb7802c68c5ed23b321418275e2
henok256 <henokmengesha1991@gmail.com>
2024-06-22 22:21:04 -0500

    code added for cicd example

---
+++ src/main/java/com/example/githubcicdactions/Application.java
@@ -0,0 +1,27 @@
+package com.example.githubcicdactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+   @GetMapping("/welcome")
+    public String hello() {
+        return "Hello, World!";
+    }
+//    echo "# github-action-example" >> README.md
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M main
+//    git remote add origin https://github.com/henok256/github-action-example.git
+//    git push -u origin main
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
